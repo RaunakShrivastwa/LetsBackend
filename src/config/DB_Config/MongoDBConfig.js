80248443332aef9a356c1df9a659c79a862027f5
RaunakShrivastwa <kumarraunak4201@gmail.com>
2025-04-01 07:35:50 +0530

    db change

--- src/config/DB_Config/MongoDBConfig.js
+++ src/config/DB_Config/MongoDBConfig.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
-mongoose.connect('mongodb+srv://canTech:220831@cantechnology.v9rfpa6.mongodb.net/Can');
+mongoose.connect('mongodb+srv://canTech:220831@cantechnology.v9rfpa6.mongodb.net/letswork');
 const DB = mongoose.connection;
 
 DB.on('error', (err) => console.log(`There is Error IN DB ${err}`));
